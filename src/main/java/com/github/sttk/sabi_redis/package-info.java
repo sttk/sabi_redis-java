def1b2fc55934203c140cc02f8f76ed64f4be48d
Takayuki Sato <sttk.xslet@gmail.com>
2026-04-05 10:21:17 +0900

    chore: scaffolded this repository (#1)

---
+++ src/main/java/com/github/sttk/sabi_redis/package-info.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2026 Takayuki Sato. All Rights Reserved.
+ *
+ * This program is free software under MIT License. See the file LICENSE in this distribution for
+ * more details.
+ */
+
+/**
+ * Provides classes to connect and operate Redis server for Sabi framework.
+ *
+ * <p>This package contains the {@code DataSrc} and {@code DataConn} classes which required to
+ * connect to and operate Redis server in various configurations.</p>
+ *
+ * @version 0.1
+ */
+package com.github.sttk.sabi_redis;
