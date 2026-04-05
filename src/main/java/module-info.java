df167a12c94c9516c1a705f1de7c37660971d80a
sttk <sttk.xslet@gmail.com>
2026-04-05 10:17:58 +0900

    chore: scaffolded this repository

---
+++ src/main/java/module-info.java
@@ -0,0 +1,19 @@
+/*
+ * module-info.java
+ * Copyright (C) 2026 Takayuki Sato. All Rights Reserved.
+ */
+
+/**
+ * Defines the APIs of Redis data access for Sabi framework.
+ *
+ * <p>This module contains the implementations of Sabi's {@code DataSrc} and {@code DataConn}
+ * required to connect to and operate Redis servers running in various configurations.
+ *
+ * @version 0.1
+ */
+module com.github.sttk.sabi_redis {
+  exports com.github.sttk.sabi_redis;
+
+  requires transitive com.github.sttk.errs;
+  requires transitive com.github.sttk.sabi;
+}
